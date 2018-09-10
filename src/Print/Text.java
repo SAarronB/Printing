7b091976b739735091a03fa41c6ae8dfde803a39
SAaronB <programmingsean@gmail.com>
2018-09-10 07:52:16 -0600

    Found Problem in Scanners

--- src/Print/Text.java
+++ src/Print/Text.java
@@ -4,8 +4,10 @@ package Print;
 import java.util.Scanner;
 
 public class Text
+
 {
 	public void start()
+	
 	{
 //Println is a way to print the entire text on one line
 		System.out.println("Standard Output for Java :)");
@@ -14,59 +16,100 @@ public class Text
 		System.out.print("OH boy more ");
 		System.out.println("Words ");
 		
-//Calling a method-------------------------------------------------------
+		
+//--------------------Calling a method--------------------------------
 		questions();
-		calculator();
-//Calling a method-------------------------------------------------------
+//--------------------Calling a method---------------------------------
 	}
 	
-	public String questions() 
+	
+	
+	public void questions() 
+	
 	{
 		
-//ASKES NAME-------------------------------------------------------------
+//----------------------ASKES NAME--------------------------------------
 		System.out.print("What is your name?");
 //This is scanning any input and will take it in
 		Scanner inputScanner = new Scanner(System.in);
 		String nameAnswer = inputScanner.nextLine();
 //Printing out the answer that was taken
 		System.out.println("Your Name is: " + nameAnswer);
-//RETURNS NAME-----------------------------------------------------------
+//----------------------RETURNS NAME--------------------------------------
 		
 		
-//ASKS PROGRAMMING LANGUAGE----------------------------------------------
+		
+		
+//---------------------ASKS PROGRAMMING LANGUAGE--------------------------
 		System.out.print("What is your favorite Programming Language?");
+		
 //This is scanning any input and will take it in
 		Scanner inputScanner2 = new Scanner(System.in);
 		String langAnswer = inputScanner2.next();
+		
 //Printing out the answer that was taken
 		System.out.println("Your Favorite Language is " + langAnswer);
 		System.out.println(nameAnswer + " likes " + langAnswer);
-//RETURNES PROGRAMMING LANGUAGE AND NAME---------------------------------
+//----------------RETURNES PROGRAMMING LANGUAGE AND NAME-------------------
+	
 		
+	
 		
-//ASKS TO USE THE CALCULATOR---------------------------------------------
+//----------------ASKS TO USE THE CALCULATOR------------------------+
 		System.out.print("Would you like to use a Calculator?");
 		Scanner inputScanner3 = new Scanner(System.in);
-		String calAnswer = inputScanner3.next();
+		String calAnswer = inputScanner3.nextLine();
+
 		System.out.println(calAnswer);
-//ASKS TO USE THE CALCULATOR---------------------------------------------
+//----------------ASKS TO USE THE CALCULATOR------------------------+
 		
-		//For SECURITY this closes the Scanner so it CANT GET ANY INPUT
-				inputScanner.close();
-				inputScanner2.close();
-				inputScanner3.close();
+
+//For SECURITY this closes the Scanner so it CANT GET ANY INPUT
+		inputScanner.close();
+		inputScanner2.close();
+		inputScanner3.close();
+//-----------------------------------------------------------------------
 		
-		//if(calAnswer = ("y") || ("yes")) {
-			return "";
-		//}
+		
+//---------------THIS INTRODUCES AND ASKES FOR ONE NUMBER TO ADD---------		
+		if(calAnswer.equals ("y") || calAnswer.equals ("yes")) 
+		
+		{
+			System.out.println("Welcome to the Calculator (BETA)");
+			System.out.println("Input first number:>");
+			Scanner inputScanner4 = new Scanner(System.in);
+			
+			//PROBLEM
+			String addAnswer1 = inputScanner4.nextLine();
+			
+//-----------------------------------------------------------------------
 
-
+			
+			
+			
+//THIS ASKES FOR THE SECOND NUMBER TO ADD AND PASSES IT TO CALCULATOR METHOD
+			System.out.println("Input second number:>");
+			Scanner inputScanner5 = new Scanner(System.in);
+			int addAnswer2 = inputScanner5.nextInt();
+			inputScanner5.close();
+			//calculator(addAnswer1,addAnswer2);
+//For SECURITY this closes the Scanner so it CANT GET ANY INPUT
+			inputScanner4.close();
+			inputScanner5.close();
+//------------------------------------------------------------------------
+		} else {
+			System.out.println("Okay then :(");
 	}
+}
 	
-	public int calculator() {
-		
-		
-		
-		return -1;
+	
+	
+//------------------------------CALCULATOR--------------------------------
+	public int calculator(int one, int two) 
+	
+	{
+		int mathAnswer = one + two;
+		return mathAnswer;
 	}
+//------------------------------CALCULATOR-------------------------------
 }
